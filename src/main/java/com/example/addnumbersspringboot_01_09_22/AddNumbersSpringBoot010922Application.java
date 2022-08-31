8c60ec9adb8c61b7e753ebe29576beb9be18d15a
Linding3r <thomasjuullindinger@yahoo.dk>
2022-08-31 18:00:40 +0200

    add two numbers

---
+++ src/main/java/com/example/addnumbersspringboot_01_09_22/AddNumbersSpringBoot010922Application.java
@@ -0,0 +1,13 @@
+package com.example.addnumbersspringboot_01_09_22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddNumbersSpringBoot010922Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AddNumbersSpringBoot010922Application.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/addnumbersspringboot_01_09_22/AddNumbersSpringBoot010922ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.addnumbersspringboot_01_09_22;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddNumbersSpringBoot010922ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
